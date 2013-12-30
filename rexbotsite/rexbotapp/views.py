a171f0f6741622c44a224220e7a0296bb8026283
bochelord <bochelord@gmail.com>
2013-12-30 13:16:02 +0100

    Django app created

---
+++ rexbotsite/rexbotapp/views.py
@@ -0,0 +1,13 @@
+
+# ReXbot
+# (c)2014 Radical Graphics Studios
+# Made with effort in Amsterdam
+#
+from django.shortcuts import render, get_object_or_404, render_to_response
+
+
+def home(request):
+
+	context = {}
+
+	return render(request, 'home.html', context)
