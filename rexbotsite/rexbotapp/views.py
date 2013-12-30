eb4f2c7c0e6544103e7586b8cc5d902a90ebcf49
Manuel Gonzalez Garcia <leunam.san@gmail.com>
2013-12-30 15:23:06 +0100

    bter api Depth test

--- rexbotsite/rexbotapp/views.py
+++ rexbotsite/rexbotapp/views.py
@@ -5,10 +5,36 @@
 #
 from django.shortcuts import render, get_object_or_404, render_to_response
 
+import sys
+
+from public import getDepth, getTradeHistory
+from trade import TradeAPI
+from keyhandler import KeyHandler
+from common import all_currencies, all_pairs, max_digits, formatCurrency, fees, formatCurrencyDigits, \
+    truncateAmount, truncateAmountDigits, BTERConnection
+
 
 def home(request):
 
-	context = {}
+
+	pair = "ltc_btc"
+
+	asks, bids = getDepth(pair)
+
+	print ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+	print len(asks), len(bids)
+
+	
+
+	# ask = asks[0].replace("(Decimal('", "")
+
+
+	# print ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+	# print ask 
+
+	context = {'asks': asks,
+			   'bids': bids
+				}
 
 	return render(request, 'dashboard/index.html', context)
 
