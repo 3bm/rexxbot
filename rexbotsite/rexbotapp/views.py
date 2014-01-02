b68a0e95b704f2e40cf9f674daf68e8f3ce99709
bochelord <bochelord@gmail.com>
2014-01-02 15:54:19 +0100

    Removed models for the time being

--- rexbotsite/rexbotapp/views.py
+++ rexbotsite/rexbotapp/views.py
@@ -6,6 +6,7 @@
 from django.shortcuts import render, get_object_or_404, render_to_response
 
 import sys
+import simplejson
 
 from public import getDepth, getTradeHistory
 from trade import TradeAPI
@@ -45,19 +46,53 @@ def home(request):
 	USD_ticker = radical_ex_lib.getTickerfastUSD()
 	EUR_ticker = radical_ex_lib.getTickerfastEUR()
 
+	nb_element = 25
+	start_time = int(time.mktime(datetime.datetime(2014, 1, 1).timetuple()) * 1000)
+	xdata = range(nb_element)
+	xdata = map(lambda x: start_time + x * 1000000000, xdata)
+	ydata = [i + random.randint(1, 2) for i in range(nb_element)]
+	ydata2 = map(lambda x: x * 2, simplejson.dumps(asks[1]))
+	#ydata2 = simplejson.dumps(asks)
+	ydata3 = map(lambda x: x * 2, simplejson.dumps(bids[1]))
+	ydata4 = map(lambda x: x * 2, ydata)
+
+	tooltip_date = "%d %b %Y %H:%M:%S %p"
+	extra_serie = {"tooltip": {"y_start": "There are ", "y_end": " calls"},
+	           "date_format": tooltip_date}
+
+	chartdata = {
+	'x': xdata,
+	'name1': 'series 1', 'y1': ydata, 'extra1': extra_serie,
+	'name2': 'series 2', 'y2': ydata2, 'extra2': extra_serie,
+	'name3': 'series 3', 'y3': ydata3, 'extra3': extra_serie,
+	'name4': 'series 4', 'y4': ydata4, 'extra4': extra_serie
+	}
+	charttype = "lineWithFocusChart"
+	chartcontainer = 'linewithfocuschart_container'  # container name
+
+
 	context = {'asks': asks,
-			   'bids': bids,
-			   'ticker' : tickerman,
-			   'ticker2' : ticker_2,
-			   'ticker3' : ticker_3,
-			   'ticker4' : ticker_4,
-			   'pair' : pair,
-			   'pair2' : pair_2,
-			   'pair3' : pair_3,
-			   'pair4' : pair_4,
-			   'usdticker' : USD_ticker,
-			   'eurticker' : EUR_ticker,
-				}
+		   'bids': bids,
+		   'ticker' : tickerman,
+		   'ticker2' : ticker_2,
+		   'ticker3' : ticker_3,
+		   'ticker4' : ticker_4,
+		   'pair' : pair,
+		   'pair2' : pair_2,
+		   'pair3' : pair_3,
+		   'pair4' : pair_4,
+		   'usdticker' : USD_ticker,
+		   'eurticker' : EUR_ticker,
+		   'charttype': charttype,
+	        'chartdata': chartdata,
+	        'chartcontainer': chartcontainer,
+	        'extra': {
+	            'x_is_date': True,
+	            'x_axis_format': '%d %b %Y %H',
+	            'tag_script_js': True,
+	            'jquery_on_ready': True,
+	}
+			}
 
 	return render(request, 'dashboard/index.html', context)
 
